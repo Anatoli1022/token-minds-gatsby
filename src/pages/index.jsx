d988226500a63c94b1c522232a5f8f112e6278fd
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-03-04 16:54:27 +0300

    feat: adds section hero,optimized,choose,achiev...

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
 
-const HomePage = () => <Layout>Hello world!</Layout>;
+import Hero from 'components/pages/home/hero';
+import Optimized from 'components/pages/home/optimized';
+import Choose from 'components/pages/home/choose';
+import Achievement from 'components/pages/home/achievement';
+
+const HomePage = () => (
+  <Layout>
+    <Hero />
+    <Optimized />
+    <Choose />
+    <Achievement />
+  </Layout>
+);
 
 export default HomePage;
 
