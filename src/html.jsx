311442a218e3ee51ee042900fac3c1791dcdd9ff
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-03-04 18:05:20 +0300

    feat: adds fonts

--- src/html.jsx
+++ src/html.jsx
@@ -22,6 +22,12 @@ const HTML = ({
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
       />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&display=swap"
+        rel="stylesheet"
+      />
       {fontsPaths.map((fontPath, index) => (
         <link
           rel="preload"
